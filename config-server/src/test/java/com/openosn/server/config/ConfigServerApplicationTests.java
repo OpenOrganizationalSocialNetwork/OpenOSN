fe4d25142ad90df1329d9dfbc7e795ff8d9c84eb
Morteza Rezaei <morteza.rezaei@nak-mci.ir>
2023-03-10 01:31:21 +0330

    add uml

---
+++ config-server/src/test/java/com/openosn/server/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openosn.server.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
